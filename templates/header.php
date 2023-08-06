8396c0a08bb207d1a856cc5d6ece61b023d8bddf
GonFr <127186793+GonFr@users.noreply.github.com>
2023-08-06 10:45:31 +0200

    working on the upline

--- templates/header.php
+++ templates/header.php
@@ -12,7 +12,29 @@
 </head>
 
 <body>
-    
+
+<!--upline-->
+<section class="">
+
+    <div class="full-container text-center upline">
+        <ul class="row align-items-start nav nav-pills">
+            <li class="col"><a href="https://www.google.fr/maps/place/Place+de+la+Bourse/@44.8441537,-0.5771867,15.65z/data=!4m14!1m7!3m6!1s0xd55263943a50ed5:0x4981f28c44504f21!2sPont+Saint-Jean!8m2!3d44.834477!4d-0.55156!16s%2Fg%2F1ptxlc2v_!3m5!1s0xd5527d1a32e69b3:0x174e5d3e97b80b10!8m2!3d44.841525!4d-0.5698651!16s%2Fm%2F0wh8fm9?entry=ttu" target="_blank">
+            <img src="assets/images/pin-map.svg" alt="" width="16" height="16" color="white" class="bi bi-telephone" viewBox="0 0 16 16">
+            </a> 1 rue de Bordeaux
+            </li>
+            <li class="col"><a href="tel:06 12 34 56 78"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-telephone" viewBox="0 0 16 16">
+                <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                </svg></a> 0612345678
+            </li>
+            <li class="col"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-clock" viewBox="0 0 16 16">
+                <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z"/>
+                <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z"/>
+                </svg> Mardi au Samedi, midi et soir
+            </li>
+        </ul>
+    </div>
+</section>
+
     
 </body>
 </html>
