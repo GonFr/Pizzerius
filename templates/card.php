6e87ccd66f1d1eddb6802b96bdaa25125c14b2cb
GonFr <127186793+GonFr@users.noreply.github.com>
2023-09-29 15:55:04 +0200

    popo in contact

--- templates/card.php
+++ templates/card.php
@@ -7,7 +7,7 @@
             <a class="btn secondbtncustom" href="menu.php">CLIQUEZ ICI</a>
         </div>
         <div class="col">
-            <span class="hstack justify-content-center"><img class="hstack img-fluid" src="assets/images/popo.png" alt="image de contact" width="50%"></span>
+            <span class="hstack justify-content-center"><img class="hstack img-fluid" src="assets/images/contact.png" alt="image de contact" width="50%"></span>
             <span class="hstack justify-content-center textdancingscript fs-2 fw-bold">Contactez nous</span>
             <a class="btn secondbtncustom" href="contact.php">CLIQUEZ ICI</a>
         </div>
